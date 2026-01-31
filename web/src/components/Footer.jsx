052ae626ad6ab2baa9fb602a805745608a1ebe5b
sohaibshaikh385 <rr1623144@gmail.com>
2026-01-31 21:34:56 +0530

    Create Footer.jsx

---
+++ web/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ textAlign: 'center', padding: '15px 0', backgroundColor: '#000', color: '#D4AF37' }}>
+      © 2026 Happie Nails. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
